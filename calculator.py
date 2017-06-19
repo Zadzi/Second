4623ed11610b3bd26fec271559d56ca3fc18643c
Zadzi <zajac.darek92@gmail.com>
2017-06-19 12:50:54 +0200

    1

---
+++ calculator.py
@@ -0,0 +1,17 @@
+class CurrencyCalculator:
+
+    def __init__(self):
+        self.euro_exchange_rate = 0
+        self.pound_exchange_rate = 0
+
+    def setEuroExchangeRate(self, value):
+        self.euro_exchange_rate = value
+
+    def setPoundExchangeRate(self, value):
+        self.pound_exchange_rate = value
+
+    def eurosToPLN(self, ammount):
+        return self.euro_exchange_rate * ammount
+
+    def poundsToPLN(self, ammount):
+        return self.pound_exchange_rate * ammount
